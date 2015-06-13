1bc33de0462da24caec8f771c2d3518119ad7947
Jonathan Wilde <hello@jwilde.me>
2015-06-13 00:15:21 -0700

    Clean up dependencies.

--- helpers/fetchGitHubArchiveAndDeploy.js
+++ helpers/fetchGitHubArchiveAndDeploy.js
@@ -1,10 +1,10 @@
-var request = require('request');
 var config = require('../config');
-var path = require('path');
-var mkdirp = require('mkdirp');
-var tar = require('tar-stream');
-var gunzip = require('gunzip-maybe');
 var fs = require('fs');
+var gunzip = require('gunzip-maybe');
+var mkdirp = require('mkdirp');
+var path = require('path');
+var request = require('request');
+var tar = require('tar-stream');
 
 var Promise = require('bluebird');
 
